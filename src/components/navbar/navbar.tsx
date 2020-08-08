dc6d4c3a9ada05ad83be8c8b60a1ec7d402ccc3c
Ameer Jhan <ameerjhanprof@gmail.com>
2020-08-08 21:31:15 +0530

    feat: add github link

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Box, Flex, Stack, useColorMode, IconButton, Image, Text } from '@chakra-ui/core';
-import { FaSignOutAlt } from 'react-icons/fa';
+import { Box, Flex, Stack, useColorMode, IconButton, Link as CLink } from '@chakra-ui/core';
+import { FaSignOutAlt, FaGithub } from 'react-icons/fa';
 import Brand from './components/brand/brand';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -10,7 +11,12 @@ export default function Navbar() {
     <Box position="fixed" width="100%" borderBottomWidth="1px" top={0} px={2} py={2}>
       <Flex justifyContent="space-between" alignItems="center">
         <Brand />
-        <Stack direction="row" spacing={1} alignItems="center">
+        <Stack direction="row" spacing={2} alignItems="center">
+          <Box 
+            size="8" as={FaGithub} 
+            cursor="pointer" 
+            onClick={() => window.open('https://github.com/ameerthehacker/github-analytics')} 
+          />
           <IconButton onClick={() => toggleColorMode()} icon={colorMode === 'light'? 'moon': 'sun'} aria-label={`Switch to ${colorMode} mode`} />
           <IconButton icon={FaSignOutAlt} aria-label="logout" />
         </Stack>
