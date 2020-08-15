7abd984b6058c17e5e4b3cd1db52a3a811f1c5d1
Ameer Jhan <ameerjhanprof@gmail.com>
2020-08-15 20:24:55 +0530

    feat: add tool tip to navbar items

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Box, Flex, Stack, useColorMode, IconButton } from '@chakra-ui/core';
+import {
+  Box,
+  Flex,
+  Stack,
+  useColorMode,
+  IconButton,
+  Tooltip,
+} from '@chakra-ui/core';
 import { FaSignOutAlt, FaGithub, FaSignInAlt } from 'react-icons/fa';
 import Brand from './components/brand/brand';
 import { useAuth } from '../../hooks/use-auth/use-auth';
@@ -30,28 +37,42 @@ export default function Navbar() {
               window.open('https://github.com/ameerthehacker/github-analytics')
             }
           />
-          <IconButton
-            onClick={() => toggleColorMode()}
-            icon={colorMode === 'light' ? 'moon' : 'sun'}
-            aria-label={`Switch to ${colorMode} mode`}
-          />
-          {isLoggedIn ? (
-            <IconButton
-              onClick={() => {
-                logout();
+          <Box>
+            <Tooltip
+              aria-label={`Switch to ${colorMode} mode`}
+              label={colorMode === 'light' ? 'nox' : 'lumos'}
+              placement="bottom"
+            >
+              <IconButton
+                onClick={() => toggleColorMode()}
+                icon={colorMode === 'light' ? 'moon' : 'sun'}
+                aria-label={`Switch to ${colorMode} mode`}
+              />
+            </Tooltip>
+          </Box>
+          <Box>
+            {isLoggedIn ? (
+              <Tooltip aria-label="logout" label="logout" placement="bottom">
+                <IconButton
+                  onClick={() => {
+                    logout();
 
-                history.push('/login');
-              }}
-              icon={FaSignOutAlt}
-              aria-label="logout"
-            />
-          ) : (
-            <IconButton
-              onClick={() => history.push('/login')}
-              icon={FaSignInAlt}
-              aria-label="login"
-            />
-          )}
+                    history.push('/login');
+                  }}
+                  icon={FaSignOutAlt}
+                  aria-label="logout"
+                />
+              </Tooltip>
+            ) : (
+              <Tooltip aria-label="login" label="login" placement="bottom">
+                <IconButton
+                  onClick={() => history.push('/login')}
+                  icon={FaSignInAlt}
+                  aria-label="login"
+                />
+              </Tooltip>
+            )}
+          </Box>
         </Stack>
       </Flex>
     </Box>
